b2cd5fceab6163cdb500c409edf2dd04e062a653
BinitaDuttaDec03 <binitadutta777@gmail.com>
2025-12-06 14:18:01 +0530

    Tuples and Membership Testing

---
+++ gen_and_agentic_ai_with_python/04_tuples_and_memebership_testing/pract.py
@@ -0,0 +1,19 @@
+masala_spices = ("cardamom", "cloves", "cinnamon")
+
+# (spice1, spice2, spice3) = masala_spices
+# print(f"Main masala spices: {spice1}, {spice2}, {spice3}")
+
+
+# ginger_ratio, cardamom_ratio = 2, 1
+# print(
+#     f"Ratio of ginger = {ginger_ratio}, Ratio of cardamom = {cardamom_ratio}")
+
+# ginger_ratio, cardamom_ratio = cardamom_ratio, ginger_ratio  # swapping
+# print(
+#     f"Ratio of ginger = {ginger_ratio}, Ratio of cardamom = {cardamom_ratio}")
+
+
+# False
+print(f"Is ginger present in masala spices? {'ginger' in masala_spices}")
+# True
+print(f"Is cinnamon present in masala spices? {'cinnamon' in masala_spices}")
