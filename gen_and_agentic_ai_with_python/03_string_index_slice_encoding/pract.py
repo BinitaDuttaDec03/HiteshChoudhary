9fa53b5e60436a3a69118a466c24f080f3d68d22
BinitaDuttaDec03 <binitadutta777@gmail.com>
2025-12-06 14:01:39 +0530

    Strings - Index, slice, encoding

---
+++ gen_and_agentic_ai_with_python/03_string_index_slice_encoding/pract.py
@@ -0,0 +1,23 @@
+# chai_type = "Ginger Tea"
+# cust_name = "Binita"
+
+# print(f"{cust_name} has ordered for a {chai_type}")
+
+
+# chai_desc = "Aromatic and Bold"
+# print(f"First word = {chai_desc[0:8]}")
+# print(f"First word = {chai_desc[:8]}")
+# print(f"First word = {chai_desc[0:8:2]}")
+
+# print(f"Last word = {chai_desc[12:]}")
+
+# print(f"Reversed string = {chai_desc[::-1]}")
+
+
+label_text = "Chai Spécial"
+label_text_encoded = label_text.encode("utf-8")
+label_text_decoded = label_text_encoded.decode("utf-8")
+
+print(f"Original text - {label_text}")
+print(f"Encoded text - {label_text_encoded}")
+print(f"Decoded text - {label_text_decoded}")
